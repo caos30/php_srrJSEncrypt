9f58ff645b398ed4143b7715d6abc02c44cb6d59
Sergi Rodrigues <sergio.cero@gmail.com>
2016-11-13 21:58:20 -0700

    Added favicons.

--- index.php
+++ index.php
@@ -51,7 +51,18 @@ if (isset($_POST['op']) and $_POST['op']=='save'){
 
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<link rel="SHORTCUT ICON" href="images/favicon.ico" />
+<!-- favicons generated at https://realfavicongenerator.net -->
+
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="images/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="images/favicon/manifest.json">
+    <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="images/favicon/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="JSEncrypt">
+    <meta name="application-name" content="JSEncrypt">
+    <meta name="msapplication-config" content="images/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 
 <link href="images/estilos.css" rel="stylesheet" type="text/css" />
 
